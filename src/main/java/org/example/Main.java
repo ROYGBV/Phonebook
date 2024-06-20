b98572cffb82caa585a0be63cdb3afa443e5f3e9
ROYGBV <ROYGBV@ROYGBV.com>
2024-06-20 09:57:45 +0300

    add description

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,13 @@
 package org.example;
 
+/**
+ * Класс для запуска приложения.
+ */
 public class Main {
+    /**
+     * Метод для запуска приложения.
+     * @param args Аргументы коммандной строки.
+     */
     public static void main(String[] args) {
         App.main(args);
     }
