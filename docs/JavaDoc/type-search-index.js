35c4aaa7cb28b0db4be4e68270ace9b67971b06a
ROYGBV <ROYGBV@ROYGBV.com>
2024-06-20 10:48:56 +0300

    add JavaDoc

---
+++ docs/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.example","l":"App"},{"p":"org.example","l":"Contact"},{"p":"org.example","l":"CorruptFileException"},{"p":"org.example","l":"Database"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"PhoneNumber"}];updateSearchResults();
